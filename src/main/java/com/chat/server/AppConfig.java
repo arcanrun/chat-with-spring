60b8bf4b7d84145abba9c019706f077826a72742
arcanrun <arcan.run@gmail.com>
2020-08-22 15:46:57 +0300

    spring: spring class config + annotations

---
+++ src/main/java/com/chat/server/AppConfig.java
@@ -0,0 +1,10 @@
+package com.chat.server;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.chat.server")
+public class AppConfig {
+}
--- src/main/java/com/chat/server/ServerRunner.java
+++ src/main/java/com/chat/server/ServerRunner.java
@@ -1,11 +1,13 @@
 package com.chat.server;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ServerRunner {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("context.config.xml");
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("context.config.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         Server server = applicationContext.getBean("server", Server.class);
         server.start();
     }
